bae526773a99a41ea94fec9ee351b42b8cd8661c
Zack Elliott <hubridnoxx@gmail.com>
2015-04-14 16:10:53 -0400

    started css

---
+++ app/app.js
@@ -0,0 +1,7 @@
+angular.module('app', [
+  'app.config',
+  // 'app.auth',
+  // 'app.game',
+  // 'app.settings',
+  // 'app.stats'
+]);
---
+++ app/config.js
@@ -0,0 +1,2 @@
+angular.module('app.config', [])
+  .constant('FBURL', 'https://nback.firebaseio.com');
